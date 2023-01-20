66e60a33a59f4f7bf33d8c633931ba7b59b17646
Emerson Elvis <121317662+EmersonElvis@users.noreply.github.com>
2023-01-20 16:32:16 -0500

    Update Ejercicio_17.1.py

--- Ejercicio_17.1.py
+++ Ejercicio_17.1.py
@@ -1,32 +1,31 @@
 #Realizar operaciones de unión, intersección y diferencia de 2 conjuntos
 print("\tCreando dos conjuntos")
-
+#Funcion crea y agrega conjunto
 def crea_conjunto(n):
     conjunto = set()
     for i in range (n):
         elemento = int(input(f"Elemento solamentos numeros enteros {i}: "))
         conjunto.add(elemento)
     return conjunto
-
+#Primer conjunto
 conjunto_1 = int(input("\nCuantos elementos quiere agregar al conjunto_1: "))
 primer_conjunto = crea_conjunto(conjunto_1)
 print(primer_conjunto)
-
+#segundo conjunto
 conjunto_2 = int(input("\nCuantos elementos quiere agregar al conjunto_2: "))
 segundo_conjunto = crea_conjunto(conjunto_2)
 print(segundo_conjunto)
-
+#Realizando union de conjuntos
 print("\tRealizando operaciones\n\t Union de conjuntos")
 union=primer_conjunto.union(segundo_conjunto)
 print("Union de conjuntos 1 y 2 es: ", union)
-
+#Realizando interseccion de conjuntos
 print("\n\tInterseccion de conjuntos")
 interseccion = primer_conjunto.intersection(segundo_conjunto)
 print("Interseccion de los conjuntos 1 y 2 es: ", interseccion)
-
+#Realizando diferencia de conjuntos
 print("\n\t Diferencia de conjuntos")
 diferencia_1 = primer_conjunto - segundo_conjunto
 print("Diferencia entre el primer conjunto y segundo es: ", diferencia_1)
-
 diferencia_2 = segundo_conjunto - primer_conjunto
 print("Diferencia entre el segundo conjunto y primer es: ", diferencia_2)
